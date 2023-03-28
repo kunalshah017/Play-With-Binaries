bd8f506a362b9a7c572c14c5454ca4086f7d5d53
Kunal Shah <95049390+kunalshah017@users.noreply.github.com>
2023-03-28 19:23:31 +0530

    Create add_contributors.sh

---
+++ add_contributors.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get the repository name and owner
+repository=`basename $(git rev-parse --show-toplevel)`
+owner=`basename $(dirname $(git rev-parse --show-toplevel))`
+
+# Get the list of contributors
+contributors=`curl -s https://api.github.com/repos/$owner/$repository/contributors?per_page=1000 | grep '"login":' | awk -F'"' '{print "- @"$4}'`
+
+# Update the README.md file with the list of contributors
+sed -i -e "/<!--START_SECTION:contributors-->/, /<!--END_SECTION:contributors-->/c\\
+<!--START_SECTION:contributors-->\\
+$contributors\\
+<!--END_SECTION:contributors-->\\
+" README.md
