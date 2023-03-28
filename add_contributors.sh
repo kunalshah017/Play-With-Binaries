21c4fce4370bbb46e556bfaba6b5a3d2d2f338e9
Kunal Shah <95049390+kunalshah017@users.noreply.github.com>
2023-03-28 19:25:25 +0530

    Update add_contributors.sh

--- add_contributors.sh
+++ add_contributors.sh
@@ -5,7 +5,7 @@ repository=`basename $(git rev-parse --show-toplevel)`
 owner=`basename $(dirname $(git rev-parse --show-toplevel))`
 
 # Get the list of contributors
-contributors=`curl -s https://api.github.com/repos/$owner/$repository/contributors?per_page=1000 | grep '"login":' | awk -F'"' '{print "- @"$4}'`
+contributors=`curl -s https://api.github.com/repos/kunalshah017/Play-With-Binaries/contributors?per_page=1000 | grep '"login":' | awk -F'"' '{print "- @"$4}'`
 
 # Update the README.md file with the list of contributors
 sed -i -e "/<!--START_SECTION:contributors-->/, /<!--END_SECTION:contributors-->/c\\
